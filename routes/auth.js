fb9048a08e08456ea4329ef73c0fe743222b1ad4
Hilary Titus Naor <hilarytn@gmail.com>
2023-12-14 15:52:07 +0100

    Set the multer file-upload route

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,9 @@
 import express from 'express';
-import {register} from '../controllers/AuthController.js'
+import upload from '../middleware/fileUpload.js';
+import {register, profilePic} from '../controllers/AuthController.js'
 const router = express.Router();
 
 router.post('/user/register', register);
+router.post('/profile-pic', upload.single('uploaded_file'), profilePic);
 
 export default router;
